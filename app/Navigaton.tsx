1f178d2a690d6f9c240369a41296c82311eda2cf
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-18 00:17:30 +0600

    nav w

--- app/Navigaton.tsx
+++ app/Navigaton.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-
+import Link from 'next/link';
 const Navigaton = () => {
   return (
-    <div className="bg-slate-600 h-16">
-      <ul>
-        <li>Home</li>
-        <li>Service</li>
-        <li>Home</li>
-        <li>Home</li>
+    <div className="bg-slate-600 h-16 flex justify-between items-center px-2">
+      <h1 className="text-white text-2xl font-bold">Mohammad</h1>
+      <ul className="flex text-slate-300  ">
+        <li className="mx-2 text-xl cursor-pointer hover:text-white transform duration-500 ">
+          <Link href="">Home</Link>
+        </li>
+        <li className="mx-2 text-xl cursor-pointer hover:text-white transform duration-500 ">
+          <Link href="">Services</Link>
+        </li>
+        <li className="mx-2 text-xl cursor-pointer hover:text-white transform duration-500 ">
+          <Link href="">Blog</Link>
+        </li>
+        <li className="mx-2 text-xl cursor-pointer hover:text-white transform duration-500 ">
+          <Link href="">Pages</Link>
+        </li>
+        <li className="mx-2 text-xl cursor-pointer hover:text-white transform duration-500 ">
+          <Link href="">Contact</Link>
+        </li>
       </ul>
+      <div className='text-white border px-4 py-2 border-slate-500 rounded-3xl cursor-pointer hover:bg-slate-700 transform duration-700'>Hire Me</div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import "../styles/globals.css";
 const Pages = () => {
   return (
-    <div>Pages</div>
+    <div className='bg-slate-600 h-[100vh]'>
+        <div className='Text'></div>
+        <div className='image'></div>
+    </div>
   )
 }
 
