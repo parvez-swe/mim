04a2a0f9aebcf5c7da9659250632936ad4efc5af
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-17 23:58:00 +0600

    nav w

--- app/Navigaton.tsx
+++ app/Navigaton.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Navigaton = () => {
   return (
-    <div className='bg-red-500'>
-        <ul>
-            <li>Hi</li>
-        </ul>
+    <div className="bg-slate-600 h-16">
+      <ul>
+        <li>Home</li>
+        <li>Service</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Navigaton
