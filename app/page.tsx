d988d85c3452ab8cee58b9607cfa908a33b84056
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-22 21:44:18 +0600

    bg for im

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,44 @@
 import React from "react";
 import "../styles/globals.css";
 import css from "./home.module.css";
-import BG from "../public/imran-vai/bg.jpg";
+import BG from "/public/imran-vai/310309226_1344791275928131_78698796048686728_n.jpg";
 import Image from "next/image";
 const Pages = () => {
   return (
-    <div
-      className={`${css.bg} text-white backdrop-blur-sm px-16 pt-10`}
-    >
-      
-      <h1 className="text-3xl font-bold tracking-widest">Hi There!</h1>
-      <h1 className="text-3xl font-bold ">I am Imran Hossain</h1>
-      <p>I am student of Daffodil Internation al University</p>
-    </div>
+    <>
+      <div className={`${css.bg} text-white backdrop-blur-sm px-16`}>
+        <div className="pt-[20%]">
+          <h1 className="text-3xl  ">Hi, My Name is </h1>
+          <h1 className="text-[4rem] font-bold leading-normal ">
+            Imran Hossain
+          </h1>
+          <p className="text-3xl ">
+            I am student of Daffodil Internation al University
+          </p>
+        </div>
+      </div>
+      <div className="about-container text-center">
+        <div>
+          <h1 className="about">About me</h1>
+          <p>who I am</p>
+        </div>
+        <div>
+          <div className="image-container text-center">
+            <Image
+              src={BG}
+              alt="BG"
+              width={200}
+              height={200}
+              priority={true}
+            />
+
+          </div>
+          <div className="text-container">
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
