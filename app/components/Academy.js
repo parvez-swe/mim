9495ac81a609fce25112cfedebdc3205538ed027
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-24 20:22:17 +0600

    youtube chennel added edt

--- app/components/Academy.js
+++ app/components/Academy.js
@@ -4,7 +4,7 @@ const Academy = () => {
   return (
     <div className="flex flex-wrap justify-center">
       <iframe
-      className="w-[100%] my-4 col-span-1 md:w-[25rem] mx-4"
+      className="w-[100%] my-4 col-span-1 md:w-[25rem] mx-2 md:mx-4"
         width="560"
         height="315"
         src="https://www.youtube.com/embed/cPZrPwxx4vo"
@@ -14,7 +14,7 @@ const Academy = () => {
         allowfullscreen
       ></iframe>
       <iframe
-      className="w-[100%] my-4 md:w-[25rem]"
+      className="w-[100%] my-4 md:w-[25rem] mx-2 md:mx-4"
         width="560"
         height="315"
         src="https://www.youtube.com/embed/lrJiRlV351o"
@@ -24,7 +24,7 @@ const Academy = () => {
         allowfullscreen
       ></iframe>
       <iframe
-      className="w-[100%] my-4 md:w-[25rem] mx-4"
+      className="w-[100%] my-4 md:w-[25rem] mx-2 md:mx-4"
         width="560"
         height="315"
         src="https://www.youtube.com/embed/X1L-TKsTuKs"
@@ -34,7 +34,7 @@ const Academy = () => {
         allowfullscreen
       ></iframe>
       <iframe
-      className="w-[100%] my-4 md:w-[25rem] mx-4"
+      className="w-[100%] my-4 md:w-[25rem] mx-2 md:mx-4"
         width="560"
         height="315"
         src="https://www.youtube.com/embed/-bjRrTB8mks"
